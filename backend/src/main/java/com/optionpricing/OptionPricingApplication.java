196ba61701f5209095b6a6ef1e2ea444f6cceb6e
“Vishal <2463805p@student.gla.ac.uk>
2026-05-13 17:34:08 +0100

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/optionpricing/OptionPricingApplication.java
@@ -0,0 +1,11 @@
+package com.optionpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionPricingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OptionPricingApplication.class, args);
+    }
+}
